694d3baec1e99d45aafcc86800ce0d9c7ab642b2
Saman Waruka <t555136@scbcorp.co.th>
2024-06-17 05:23:46 +0700

    feat: implement find odd-numbers

--- typescript/odd-number.spec.ts
+++ typescript/odd-number.spec.ts
@@ -1,1 +1,33 @@
 // Write your test here
+
+import { findOddNumber } from "./odd-number";
+
+test("[7] should return 7, because it occurs 1 time (which is odd).", () => {
+  const inputs = [7];
+  const oddNumber = findOddNumber(inputs);
+  expect(oddNumber).toBe(7);
+});
+
+test("[0] should return 0, because it occurs 1 time (which is odd).", () => {
+  const inputs = [0];
+  const oddNumber = findOddNumber(inputs);
+  expect(oddNumber).toBe(0);
+});
+
+test("[1,1,2] should return 2, because it occurs 1 time (which is odd).", () => {
+  const inputs = [1, 1, 2];
+  const oddNumber = findOddNumber(inputs);
+  expect(oddNumber).toBe(2);
+});
+
+test("[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd)", () => {
+  const inputs = [0, 1, 0, 1, 0];
+  const oddNumber = findOddNumber(inputs);
+  expect(oddNumber).toBe(0);
+});
+
+test("[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).", () => {
+  const inputs = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1];
+  const oddNumber = findOddNumber(inputs);
+  expect(oddNumber).toBe(4);
+});
--- typescript/odd-number.ts
+++ typescript/odd-number.ts
@@ -1,9 +1,30 @@
 /**
- * 
+ *
  * @param numbers list of numbers example [7], [0], [1,1,2], [0,1,0,0,1], [1,2,3,4,5,6,7,8,9,0]
  * @returns one number that odd number
- */ 
-export function findOddNumber(numbers: number[]) : number {
-    // TODO : Start your code here
-    throw new Error("Not implemented");
+ */
+export function findOddNumber(numbers: number[]): number | undefined {
+  if (numbers.length === 0) {
+    throw new Error("Input array is require, and have at lease 1 element.");
+  }
+  if (numbers.length === 1) {
+    return numbers[0];
+  }
+
+  const countObj: Record<string, number> = {};
+  for (const num of numbers) {
+    if (countObj.hasOwnProperty(num)) {
+      countObj[num] = countObj[num] + 1;
+    } else {
+      countObj[num] = 1;
+    }
+  }
+
+  const countObjKeys = Object.keys(countObj);
+
+  for (const key of countObjKeys) {
+    if (countObj[key] % 2 === 1) {
+      return Number(key);
+    }
+  }
 }
