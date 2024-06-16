9bb804db06be746cf2246b2fe905a56523ee6a59
Saman Waruka <t555136@scbcorp.co.th>
2024-06-17 04:57:12 +0700

    feat: implement smilyCount

--- typescript/smily.spec.ts
+++ typescript/smily.spec.ts
@@ -1,1 +1,21 @@
 // Write your test here
+
+import { countSmilyFace } from "./smily";
+
+test("[':)', ';(', ';}', ':-D'] should return 2", () => {
+  const inputs = [":)", ";(", ";}", ":-D"];
+  const amount = countSmilyFace(inputs);
+  expect(amount).toBe(2);
+});
+
+test("[';D', ':-(', ':-)', ';~)'] should return 3", () => {
+  const inputs = [";D", ":-(", ":-)", ";~)"];
+  const amount = countSmilyFace(inputs);
+  expect(amount).toBe(3);
+});
+
+test("[';]', ':[', ';*', ':$', ';-D'] should return 1", () => {
+  const inputs = [";]", ":[", ";*", ":$", ";-D"];
+  const amount = countSmilyFace(inputs);
+  expect(amount).toBe(1);
+});
--- typescript/smily.ts
+++ typescript/smily.ts
@@ -1,9 +1,22 @@
+export const smilyRegex = /[:;][-~]?[)D]/;
+
 /**
- * 
+ *
  * @param texts list of string [":)", ":(", ":>"]
  * @returns amount of smily face detected
  */
-export function countSmilyFace(texts: string[]) : number {
-    // TODO : Start your code here
-    throw new Error("Not implemented");
+export function countSmilyFace(texts: string[]): number {
+  if (texts.length === 0) {
+    return 0;
+  }
+
+  let countSmily = 0;
+  for (const text of texts) {
+    const trimedText = text.trim();
+    if (smilyRegex.test(trimedText)) {
+      countSmily = countSmily + 1;
+    }
+  }
+
+  return countSmily;
 }
